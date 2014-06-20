ce598571bc215bcefd575786f465430f98b47049
nrock <nmacellaio@gmail.com>
2014-06-20 08:54:17 -0500

    new project web project

---
+++ Source/SecurityGuard.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecurityGuard.Web.Startup))]
+namespace SecurityGuard.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
